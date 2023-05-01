2e8795cff435d1fff917dc27c92beba6790680cd
Novi Amanda Puspita Sari <apsnovi@gmail.com>
2023-05-01 16:31:38 +0700

    update detail

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pertemuan 6 - 123200006',
+      title: 'Pertemuan 6 - 123200065',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
